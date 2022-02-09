5a3619c9da8ed83594534eaced8b29cfd9bd6ef7
Gerald Kunze <gast42_gk@ldbv.bayern.de>
2022-02-09 16:11:56 +0100

    EAB-781 Analysemenü - Änderung Schrift und Untertitel

--- src/ea/modules/menu/i18n/menu.provider.js
+++ src/ea/modules/menu/i18n/menu.provider.js
@@ -23,18 +23,18 @@ export const provide = (lang) => {
 			return {
 				//the first part of the snake_case key should be the name of the related module
 				ea_menu_report : "Mitmachen und Börsen",
-				ea_menu_report_tooltip : "Mitmachen und Börsen",
+				ea_menu_report_tooltip : "Korrekturen und neue Objekte melden",
 				ea_menu_analyse3d : "3D-Analyse Windenergieanlagen",
-				ea_menu_analyse3d_tooltip : "Hier eine realitätsnahe Darstellung von Windenergieanlagen in der Landschaft erstellen",
+				ea_menu_analyse3d_tooltip : "Realitätsnahe Darstellungen von Windenergieanlagen in der Landschaft erstellen",
 				ea_menu_recherche : "Daten-Recherche und Download",
-				ea_menu_recherche_tooltip : "Hier Daten durchsuchen und exportieren",
+				ea_menu_recherche_tooltip : "Daten durchsuchen und exportieren",
 				ea_menu_mixer : "Mischpult \"Energiemix Bayern vor Ort\"",
-				ea_menu_mixer_tooltip : "Hier Strom- und Wärmemix, Potenziale und Szenarien einer Kommune abrufen",
+				ea_menu_mixer_tooltip : "Energiemix, Potenziale und Szenarien einer Kommune abrufen",
 				ea_menu_geotherm : "Standortcheck Oberflächennahe Geothermie",
 				ea_menu_geotherm_short : "Standortcheck Oberfl. Geothermie",
-				ea_menu_geotherm_tooltip : "Hier die Standorteignung für oberflächennahe Geothermie prüfen",
+				ea_menu_geotherm_tooltip : " Standorteignung für oberflächennahe Geothermie prüfen",
 				ea_menu_redesign : "Mischpult \"Redesign\"",
-				ea_menu_redesign_tooltip : "Dieses Modul dient zur Entwicklung und Redesign der Zusatzmodule"
+				ea_menu_redesign_tooltip : "Modul für Redesign der Zusatzmodule"
 			};
 
 		default:
