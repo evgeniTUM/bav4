6e454c21cdd9344d6b105237933354358b84585d
Evgeni Pavlidis <evgeni.pavlidis@tutanota.com>
2022-03-22 15:32:12 +0100

    fixed StoreService Test

--- test/service/StoreService.test.js
+++ test/service/StoreService.test.js
@@ -86,7 +86,7 @@ describe('StoreService', () => {
 			expect(store).toBeDefined();
 
 			const reducerKeys = Object.keys(store.getState());
-			expect(reducerKeys.length).toBe(21);
+			expect(reducerKeys.length).toBe(22);
 			expect(reducerKeys.includes('map')).toBeTrue();
 			expect(reducerKeys.includes('pointer')).toBeTrue();
 			expect(reducerKeys.includes('position')).toBeTrue();
@@ -108,6 +108,7 @@ describe('StoreService', () => {
 			expect(reducerKeys.includes('featureInfo')).toBeTrue();
 			expect(reducerKeys.includes('media')).toBeTrue();
 			expect(reducerKeys.includes('import')).toBeTrue();
+			expect(reducerKeys.includes('contribute')).toBeTrue();
 		});
 
 		it('registers all plugins', (done) => {
