fa8ebe3f20d7dd05e6ed96b0cc5b4dbd8817aa21
Gerald Kunze <gast42_gk@ldbv.bayern.de>
2022-03-31 10:11:45 +0200

    Merge remote-tracking branch 'upstream/master' into dev_4 Anpassung für externe Konfiguration MODULE_BACKEND_URL

--- src/ea/services/EaProcessEnvConfigService.js
+++ src/ea/services/EaProcessEnvConfigService.js
@@ -10,6 +10,6 @@ export class EaProcessEnvConfigService extends ProcessEnvConfigService{
 	constructor() {
             super();
 		// eslint-disable-next-line no-undef
-		this._properties.set('MODULE_BACKEND_URL', process.env.MODULE_BACKEND_URL);
+		this._properties.set('MODULE_BACKEND_URL', window?.ba_externalConfigProperties?.MODULE_BACKEND_URL ?? process.env.MODULE_BACKEND_URL);
 	}
 }
