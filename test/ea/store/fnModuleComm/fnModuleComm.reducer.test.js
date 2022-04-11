f890c38d00dd1f96fd3740392b8e28b6315f1c5a
Evgeni Pavlidis <Gast42_EP@ldbv.bayern.de>
2022-04-11 14:50:32 +0200

    add reducer test for close of module

--- test/ea/store/fnModuleComm/fnModuleComm.reducer.test.js
+++ test/ea/store/fnModuleComm/fnModuleComm.reducer.test.js
@@ -1,4 +1,4 @@
-import { openFnModuleComm } from '../../../../src/ea/store/fnModuleComm/fnModuleComm.action';
+import { clearLayer, closeFnModules, openFnModuleComm } from '../../../../src/ea/store/fnModuleComm/fnModuleComm.action';
 import { fnModuleCommReducer } from '../../../../src/ea/store/fnModuleComm/fnModuleComm.reducer';
 import { TestUtils } from '../../../test-utils';
 
@@ -13,20 +13,27 @@ describe('fnModuleCommReducer', () => {
 
 	it('initiales the store with default values', () => {
 		const store = setup();
-		expect(store.getState().fnModuleComm.features).toHaveSize(0);
+		const actual = store.getState().fnModuleComm;
+
+		expect(actual.features).toHaveSize(0);
+		expect(actual.layers).toHaveSize(0);
+		expect(actual.fnModuleSite).toBeNull();
+		expect(actual.fnModuleWindow).toBeNull();
+		expect(actual.fnModuleDomain).toBeNull();
+		expect(actual.active).toBeFalse();
 	});
 
-	it('open the \'module\' and \'active\' property by adding module properties', () => {
-		const store = setup();
+	it('opens and closes the \'module\'', () => { const store = setup();
 
 		openFnModuleComm('mixer', window.location.origin, window);
 		expect(store.getState().fnModuleComm.features).toHaveSize(0);
 		expect(store.getState().fnModuleComm.fnModuleSite).toBe('mixer');
 		expect(store.getState().fnModuleComm.fnModuleWindow).not.toBeNull();
-		console.log(store.getState().fnModuleComm.fnModuleDomain);
 		expect(store.getState().fnModuleComm.fnModuleDomain).toBe(window.location.origin);
 		expect(store.getState().fnModuleComm.active).toBeTrue();
 
+		closeFnModules();
+		expect(store.getState().fnModuleComm.active).toBeFalse();
 	});
 
 });
