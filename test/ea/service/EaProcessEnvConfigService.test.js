ff9272dc856cf4bd3082454adcd9564f6ad25a5a
Evgeni Pavlidis <Gast42_EP@ldbv.bayern.de>
2022-04-12 14:30:40 +0200

    add Test for EaProcessEnvConfigService

---
+++ test/ea/service/EaProcessEnvConfigService.test.js
@@ -0,0 +1,47 @@
+import { EaProcessEnvConfigService } from '../../../src/ea/services/EaProcessEnvConfigService' 
+import { ProcessEnvConfigService } from '../../../src/services/ProcessEnvConfigService';
+
+describe('tests for EaProcessEnvConfigService', () => {
+
+	beforeEach(function () {
+		// eslint-disable-next-line no-undef
+		const process = {
+			env: {
+			}
+		};
+		window.process = process;
+		window.ba_externalConfigProperties = {};
+	});
+	it('inherits ProcessEnvConfigService', () => {
+		const object = new EaProcessEnvConfigService();
+		expect(object instanceof ProcessEnvConfigService).toBeTrue();
+	});
+
+	describe('getValue()', () => {
+
+		it('provides a value for required keys from process.env', () => {
+			// eslint-disable-next-line no-undef
+			process.env = {
+				'MODULE_BACKEND_URL': 'MODULE_BACKEND_URL_value'
+			};
+
+			const configService = new EaProcessEnvConfigService();
+
+			expect(configService.getValue('MODULE_BACKEND_URL')).toBe('MODULE_BACKEND_URL_value');
+		});
+
+		it('provides a value for required keys from window.config', () => {
+			// eslint-disable-next-line no-undef
+			window.ba_externalConfigProperties = {
+				'MODULE_BACKEND_URL': 'MODULE_BACKEND_URL_value'
+			};
+
+			const configService = new EaProcessEnvConfigService();
+
+			expect(configService.getValue('MODULE_BACKEND_URL')).toBe('MODULE_BACKEND_URL_value');
+
+		});
+
+	});
+
+});
