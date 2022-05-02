242877cae3d5b241c3dc309942dbce1faeaead4b
Evgeni Pavlidis <Gast42_EP@ldbv.bayern.de>
2022-05-02 14:49:01 +0200

    removed unused components

--- src/ea/modules/map/components/mapContainer/index.js
+++
@@ -1,4 +0,0 @@
-import { EaMapContainer } from './EaMapContainer';
-if (!window.customElements.get(EaMapContainer.tag)) {
-	window.customElements.define(EaMapContainer.tag, EaMapContainer);
-}
--- src/ea/modules/toolbox/components/toolBar/index.js
+++
@@ -1,4 +0,0 @@
-import { EaToolBar } from './EaToolBar';
-if (!window.customElements.get(EaToolBar.tag)) {
-	window.customElements.define(EaToolBar.tag, EaToolBar);
-}
