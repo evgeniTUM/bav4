5efef74a7b5520778f4e3759651cde455646d7bd
Evgeni Pavlidis <Gast42_EP@ldbv.bayern.de>
2022-07-11 11:39:30 +0200

    add capabilities service

--- src/ea/injection/eaConfig.js
+++ src/ea/injection/eaConfig.js
@@ -4,9 +4,11 @@ import { eaMapModule } from '../modules/map/injection/';
 import { FnModulePlugin } from '../plugins/FnModulePlugin';
 import { ManageModulesPlugin } from '../plugins/ManageModulesPlugin';
 import { LegendPlugin } from '../plugins/LegendPlugin';
+import { WmsCapabilitiesService } from '../services/WmsCapabilitiesService';
 
 
 $injector
+	.registerSingleton('WmsCapabilitiesService', new WmsCapabilitiesService())
 	.registerSingleton('FnModulePlugin', new FnModulePlugin())
 	.registerSingleton('ManageModulesPlugin', new ManageModulesPlugin())
 	.registerSingleton('LegendPlugin', new LegendPlugin())
