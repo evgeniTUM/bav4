e7e25a299c4256969fb9da841b4bc1f373fb2773
Evgeni Pavlidis <Gast42_EP@ldbv.bayern.de>
2022-08-09 13:13:10 +0200

    add tracking for events (tool change, layer change, module change)

--- src/ea/plugins/TrackingPlugin.js
+++ src/ea/plugins/TrackingPlugin.js
@@ -1,5 +1,6 @@
 import { $injector } from '../../injection';
 import { BaPlugin } from '../../plugins/BaPlugin';
+import { observe } from '../../utils/storeUtils';
 
 export class TrackingPlugin extends BaPlugin {
 
@@ -7,21 +8,61 @@ export class TrackingPlugin extends BaPlugin {
 	 * @override
 	 * @param {Store} store
 	 */
-	async register() {
+	async register(store) {
 		const { ConfigService: configService } = $injector.inject('ConfigService');
 
-		const matomoUrl = configService.getValue('MATOMO_URL') + '/';
-		const matomoId = configService.getValue('MATOMO_ID');
 
-		const libraryName = 'matomo';
+		const activateMatomo = () => {
+			const matomoUrl = configService.getValue('MATOMO_URL') + '/';
+			const matomoId = configService.getValue('MATOMO_ID');
 
-		const _paq = window._paq = window._paq || [];
-		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-		_paq.push(['trackPageView']);
-		_paq.push(['enableLinkTracking']);
-		_paq.push(['setTrackerUrl', matomoUrl + libraryName + '.php']);
-		_paq.push(['setSiteId', matomoId]);
-		const d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-		g.async = true; g.src = matomoUrl + libraryName + '.js'; s.parentNode.insertBefore(g, s);
+			const libraryName = 'matomo';
+
+			const _paq = window._paq = window._paq || [];
+			/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+			_paq.push(['trackPageView']);
+			_paq.push(['enableLinkTracking']);
+			_paq.push(['setTrackerUrl', matomoUrl + libraryName + '.php']);
+			_paq.push(['setSiteId', matomoId]);
+			const d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+			g.async = true; g.src = matomoUrl + libraryName + '.js'; s.parentNode.insertBefore(g, s);
+			g.setAttribute('id', 'matomo-script');
+		};
+
+		const deactivateMatomo = () => {
+			const element = document.getElementById('matomo-script');
+			element.parentNode.removeChild(element);
+			window._paq = [];
+		};
+
+
+		const trackToolChange = (toolId) => {
+			window._paq.push(['trackEvent', 'Tool', 'select', toolId]);
+		};
+
+
+		let activeLayerIdsState = [];
+		const trackLayerChange = (activeLayers) => {
+
+			const activeLayerIds = activeLayers.map(l => l.id);
+			const newIds = activeLayerIds.filter(id => !activeLayerIdsState.includes(id));
+
+			newIds.forEach(id =>
+				window._paq.push(['trackEvent', 'Layer', 'activate', id])
+			);
+
+			activeLayerIdsState = activeLayerIds;
+		};
+
+		const trackModuleChange = (moduleId) => {
+			window._paq.push(['trackEvent', 'Module', 'activate', moduleId]);
+		};
+
+
+		observe(store, state => state.ea.trackingActive, active => active ? activateMatomo() : deactivateMatomo());
+
+		observe(store, state => state.tools.current, trackToolChange);
+		observe(store, state => state.layers.active, trackLayerChange);
+		observe(store, state => state.ea.currentModule, trackModuleChange);
 	}
 }
--- test/ea/plugins/TrackingPlugin.test.js
+++ test/ea/plugins/TrackingPlugin.test.js
@@ -1,7 +1,12 @@
+import { MixerModuleContent } from '../../../src/ea/modules/toolbox/components/mixer/MixerModuleContent.js';
 import { TrackingPlugin } from '../../../src/ea/plugins/TrackingPlugin.js';
+import { activateTracking, deactivateTracking, setCurrentModule } from '../../../src/ea/store/module/ea.action.js';
 import { eaReducer } from '../../../src/ea/store/module/ea.reducer.js';
 import { $injector } from '../../../src/injection/index.js';
+import { addLayer } from '../../../src/store/layers/layers.action.js';
 import { layersReducer } from '../../../src/store/layers/layers.reducer.js';
+import { setCurrentTool, ToolId } from '../../../src/store/tools/tools.action.js';
+import { toolsReducer } from '../../../src/store/tools/tools.reducer.js';
 import { TestUtils } from '../../test-utils.js';
 
 
@@ -18,6 +23,7 @@ describe('TrackingPlugin', () => {
 		storeActions.length = 0;
 
 		const store = TestUtils.setupStoreAndDi(state, {
+			tools: toolsReducer,
 			layers: layersReducer,
 			ea: eaReducer
 		});
@@ -31,27 +37,62 @@ describe('TrackingPlugin', () => {
 		return store;
 	};
 
-	describe('initialization', () => {
 
-		it('when active, adds a script element to document and inits matomo', async () => {
-			await setup();
+	it('adds/removes matomo script element to document when enabled/disabled', async () => {
+		await setup();
 
-			const scriptElements = document.getElementsByTagName('script');
+		activateTracking();
 
-			const elements = [...scriptElements].map(n => n.outerHTML);
-			expect(elements).toContain('<script async="" src="MATOMO_URL/matomo.js"></script>');
+		let scriptElement = document.getElementById('matomo-script');
+		expect(scriptElement.outerHTML).toContain(
+			'<script async="" src="MATOMO_URL/matomo.js" id="matomo-script"></script>'
+		);
 
-			expect(window._paq).toEqual([
-				['trackPageView'],
-				['enableLinkTracking'],
-				['setTrackerUrl',
-					'MATOMO_URL/matomo.php'],
-				['setSiteId', 'MATOMO_ID']
-			]);
-		});
+		expect(window._paq).toEqual([
+			['trackPageView'],
+			['enableLinkTracking'],
+			['setTrackerUrl',
+				'MATOMO_URL/matomo.php'],
+			['setSiteId', 'MATOMO_ID']
+		]);
 
+		deactivateTracking();
+
+		scriptElement = document.getElementById('matomo-script');
+		expect(scriptElement).toBeNull();
+		expect(window._paq).toEqual([]);
 	});
 
 
+	describe('tracks user actions', () => {
 
+		beforeAll(async () => {
+			await setup();
+			activateTracking();
+		});
+
+		afterAll(async () => {
+			deactivateTracking();
+		});
+
+		it('tool selection', async () => {
+			setCurrentTool(ToolId.DRAWING);
+
+			expect(window._paq).toContain(['trackEvent', 'Tool', 'select', ToolId.DRAWING]);
+		});
+
+		it('layer activation', async () => {
+			addLayer('id1');
+			addLayer('id2');
+
+			expect(window._paq).toContain(['trackEvent', 'Layer', 'activate', 'id1']);
+			expect(window._paq).toContain(['trackEvent', 'Layer', 'activate', 'id2']);
+		});
+
+		it('module selection', async () => {
+			setCurrentModule(MixerModuleContent.tag);
+
+			expect(window._paq).toContain(['trackEvent', 'Module', 'activate', MixerModuleContent.tag]);
+		});
+	});
 });
