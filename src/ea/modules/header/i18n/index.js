d2446e6d3010a26daf26ef90689f8af3b27ae2d7
Evgeni Pavlidis <Gast42_EP@ldbv.bayern.de>
2022-09-30 13:26:29 +0200

    refactor: remove EaHeader (difficult to merge changes from BA code base)

--- src/ea/modules/header/i18n/index.js
+++
@@ -1,4 +0,0 @@
-import { provide as eaHeaderProvider } from './header.provider';
-import { $injector } from '../../../../injection';
-const { TranslationService: translationService } = $injector.inject('TranslationService');
-translationService.register('eaHeaderProvider', eaHeaderProvider);
