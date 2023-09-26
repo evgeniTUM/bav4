8806c7643e854f4dfe7d990da216efba96e8d908
Gerald Kunze <gast42_gk@ldbv.bayern.de>
2023-09-26 12:30:29 +0200

    EAB1819 BA Feedback-Funktion inegrieren und auf EAB abstimmen

--- src/ea/modules/menu/components/mainMenu/content/misc/EaMiscContentPanel.js
+++ src/ea/modules/menu/components/mainMenu/content/misc/EaMiscContentPanel.js
@@ -2,6 +2,7 @@ import { html } from 'lit-html';
 import { $injector } from '../../../../../../../injection';
 import { AbstractMvuContentPanel } from '../../../../../../../modules/menu/components/mainMenu/content/AbstractMvuContentPanel';
 import css from './eaMiscContentPanel.css';
+import { closeModal, openModal } from '../../../../../../../store/modal/modal.action';
 
 const Update_Collapsed = 'update_collapse';
 
@@ -10,7 +11,10 @@ export class EaMiscContentPanel extends AbstractMvuContentPanel {
 		super();
 
 		const { TranslationService } = $injector.inject('TranslationService');
-		this._translationService = TranslationService;
+		(this._selectedFeedbackPanel = null), (this._translationService = TranslationService);
+		this._onSubmit = () => {
+			closeModal();
+		};
 	}
 
 	update(type, data, model) {
@@ -22,6 +26,12 @@ export class EaMiscContentPanel extends AbstractMvuContentPanel {
 
 	createView() {
 		const translate = (key) => this._translationService.translate(key);
+
+		const openFeedbackDialog = () => {
+			const title = translate('menu_misc_content_panel_feedback_title');
+			const content = html`<ba-mvu-generalfeedbackpanel .onSubmit=${this._onSubmit}></ba-mvu-generalfeedbackpanel>`;
+			openModal(title, content, { steps: 1 });
+		};
 
 		const link = (link, text_id, tooltip_id, icon) =>
 			html`<a class="ba-list-item" href="${link}" title="${translate(tooltip_id)}" target="_blank">
@@ -63,16 +73,22 @@ export class EaMiscContentPanel extends AbstractMvuContentPanel {
 				</collapsable-content>
 				<collapsable-content .title=${translate('ea_menu_misc_content_panel_aboutus')}>
 					${link(
-						'https://www.energieatlas.bayern.de/service/impressum.html',
-						'ea_menu_misc_content_panel_imprint',
-						'ea_menu_misc_content_panel_imprint_tooltip',
-						'imprint'
-					)}
-					${link(
 						'https://www.energieatlas.bayern.de/service/kontakt.html',
 						'ea_menu_misc_content_panel_contact',
 						'ea_menu_misc_content_panel_contact_tooltip',
 						'contact'
+					)}
+					<button id="feedbackGeneralButton" class="ba-list-item" @click=${openFeedbackDialog}>
+						<span class="ba-list-item__pre ">
+							<span class="ba-list-item__icon link-icon feedback"> </span>
+						</span>
+						<span class="ba-list-item__text vertical-center">${translate('feedback_generalFeedback')}</span>
+					</button>
+					${link(
+						'https://www.energieatlas.bayern.de/service/impressum.html',
+						'ea_menu_misc_content_panel_imprint',
+						'ea_menu_misc_content_panel_imprint_tooltip',
+						'imprint'
 					)}
 				</collapsable-content>
 				<collapsable-content .title=${translate('ea_menu_misc_content_panel_legal')}>
@@ -118,6 +134,10 @@ export class EaMiscContentPanel extends AbstractMvuContentPanel {
 					)}
 				</collapsable-content>
 			</div>`;
+	}
+
+	set onSubmit(callback) {
+		this._onSubmit = callback;
 	}
 
 	static get tag() {
--- src/modules/feedback/i18n/feedback.provider.js
+++ src/modules/feedback/i18n/feedback.provider.js
@@ -7,7 +7,7 @@ export const provide = (lang) => {
 				feedback_categorySelection_error: 'required field',
 				feedback_changeDescription: 'Your message',
 				feedback_disclaimer:
-					'In some cases, the LDBV cannot adopt your feedback. For questions relating to your feedback and to keep you informed about the work in progress we recommend submitting your email address as well.',
+					'For questions relating to your feedback and to keep you informed about the work in progress we recommend submitting your email address as well',
 				feedback_eMail_error: 'No valid e-mail address',
 				feedback_eMail_helper: 'Optional',
 				feedback_eMail: 'Your email address',
@@ -28,7 +28,8 @@ export const provide = (lang) => {
 				feedback_mapFeedback_text_after: ' and fill out the form. Your message will be displayed publicly in the feedback window in a timely manner.',
 				feedback_mapFeedback_text_before: 'Please mark the place where you would like to make a correction suggestion in the',
 				feedback_mapFeedback_text_map: 'map',
-				feedback_mapFeedback: 'Map - Feedback'
+				feedback_mapFeedback: 'Map - Feedback',
+				feedback_privacyPolicy_url: 'https://www.energieatlas.bayern.de/datenschutz'
 			};
 
 		case 'de':
@@ -37,20 +38,19 @@ export const provide = (lang) => {
 				feedback_categorySelection_helper: 'Bitte auswählen,',
 				feedback_categorySelection_error: 'Pflichtfeld',
 				feedback_changeDescription: 'Ihre Nachricht',
-				feedback_disclaimer:
-					'Das LDBV behält sich grundsätzlich vor, Meldungen nicht zu übernehmen. Für evtl. Rückfragen, sowie zur Information über die weitere Bearbeitung, empfehlen wir die Angabe Ihrer E-Mail-Adresse.',
+				feedback_disclaimer: 'Für evtl. Rückfragen sowie zur Information über die weitere Bearbeitung empfehlen wir die Angabe Ihrer E-Mail-Adresse',
 				feedback_eMail_error: 'Keine gültige E-Mail-Adresse',
 				feedback_eMail_helper: 'Optional',
 				feedback_eMail: 'Ihre E-Mail-Adresse',
 				feedback_privacyPolicy: 'Hinweis zum Datenschutz',
 				feedback_required_field_error: 'Pflichtfeld',
 				feedback_required_field_helper: 'Pflichtfeld',
-				feedback_saved_successfully: 'Ihr Feedback wurde gespeichert',
+				feedback_saved_successfully: 'Ihr Feedback wurde erfolgreich versendet',
 				feedback_submit: 'Senden',
 				feedback_toggleFeedback_generalButton_sub: 'Machen Sie Änderungs- oder Verbesserungsvorschläge oder teilen Sie uns Ihre Meinung mit.',
 				feedback_toggleFeedback_mapButton_sub: 'Melden Sie uns Korrekturvorschläge zu den Karteninhalten.',
 				feedback_generalFeedback_could_not_save: 'Ihr Feedback konnte nicht gespeichert werden',
-				feedback_generalFeedback_rating: 'Würden Sie den BayernAtlas weiterempfehlen?',
+				feedback_generalFeedback_rating: 'Würden Sie den  Energie-Atlas Bayern weiterempfehlen?',
 				feedback_generalFeedback_rating_scale_0: 'Sehr wahrscheinlich',
 				feedback_generalFeedback_rating_scale_5: 'Sehr unwahrscheinlich',
 				feedback_generalFeedback: 'Allgemeines Feedback',
@@ -59,7 +59,8 @@ export const provide = (lang) => {
 				feedback_mapFeedback_text_after: ' und füllen das Formular aus. Ihre Meldung wird zeitnah öffentlich im Feedback-Fenster angezeigt.',
 				feedback_mapFeedback_text_before: 'Markieren Sie bitte die Stelle, an der Sie einen Korrekturvorschlag machen möchten, in der',
 				feedback_mapFeedback_text_map: 'Karte',
-				feedback_mapFeedback: 'Feedback zur Karte'
+				feedback_mapFeedback: 'Feedback zur Karte',
+				feedback_privacyPolicy_url: 'https://www.energieatlas.bayern.de/datenschutz'
 			};
 
 		default:
